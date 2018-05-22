0b45b8c45cedc27a622d3abe1f129294d9d514a6
Evgeniy <evgeniy@stereoflo.ru>
2018-05-22 12:08:30 +0300

    cover remove fix

--- app/Http/Controllers/ManagerController.php
+++ app/Http/Controllers/ManagerController.php
@@ -90,7 +90,7 @@ class ManagerController extends Controller
     public function coverRemove(Project $project, int $id)
     {
         $project->coverRemove($id);
-        return redirect('/manager');
+        return redirect(route('managerProjectEdit', ['id' => $id]));
     }
 
     /**
